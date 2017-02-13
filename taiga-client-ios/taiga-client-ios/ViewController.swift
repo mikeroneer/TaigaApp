9fa0ed2099649ea5caa498d4d5f846fef05787c4
Dominik Reichinger <reichinger.dominik@gmail.com>
2017-02-13 10:22:29 +0100

    Created xcode project

---
+++ taiga-client-ios/taiga-client-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taiga-client-ios
+//
+//  Created by Dominik on 13.02.17.
+//  Copyright © 2017 r31r0c. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
