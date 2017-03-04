359cc047d052fbf3bfcf6d7df5bbad88f30b0513
Dominik Reichinger <reichinger.dominik@gmail.com>
2017-03-04 11:35:59 +0100

    userstory detail

--- taiga-client-ios/taiga-client-ios/StoryDetailsViewController.swift
+++ taiga-client-ios/taiga-client-ios/StoryDetailsViewController.swift
@@ -11,8 +11,35 @@ import UIKit
 class StoryDetailsViewController: UIViewController {
     var userstory: UserStoryDetail?
     
+    
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var textDescription: UITextView!
+    @IBOutlet weak var lblCreated: UILabel!
+    @IBOutlet weak var lblStatus: UILabel!
+    @IBOutlet weak var lblAssignedTo: UILabel!
+    @IBOutlet weak var lblTotalPoints: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let details = userstory {
+            lblName.text = details.subject
+            
+            if details.description.isEmpty {
+                textDescription.text = "No description"
+            } else {
+                textDescription.text = details.description
+            }
+            
+            lblCreated.text = details.createdDate
+            lblStatus.text = details.statusExtraInfo.name
+            if details.assignedTo.isEmpty {
+                lblAssignedTo.text = "Not assigned"
+            } else {
+                lblAssignedTo.text = details.assignedTo
+            }
+            lblTotalPoints.text = "\(details.totalPoints) total points"
+        }
     }
     
     override func didReceiveMemoryWarning() {
--- taiga-client-ios/taiga-client-ios/UserStoryDetailGET.swift
+++ taiga-client-ios/taiga-client-ios/UserStoryDetailGET.swift
@@ -1,16 +1,14 @@
 //
-//  UserStoryDetail.swift
+//  UserStoryDetailGET.swift
 //  taiga-client-ios
 //
-//  Created by Michael Rockenschaub on 27/02/2017.
+//  Created by Dominik on 04.03.17.
 //  Copyright © 2017 r31r0c. All rights reserved.
 //
 
 import SwiftyJSON
 
-class UserStoryDetail {
-    let createdDate: String
-    let description: String
+class UserStoryDetailGET {
     let id: Int
     let ref: Int
     let subject: String
@@ -19,10 +17,9 @@ class UserStoryDetail {
     let comment: String
     let totalPoints: Double
     let statusExtraInfo: StatusExtraInfo
+    let assignedToExtraInfo: AssignedToExtraInfo
     
     init(json: JSON) {
-        createdDate = json["created_date"].stringValue
-        description = json["description"].stringValue
         id = json["id"].intValue
         subject = json["subject"].stringValue
         ref = json["ref"].intValue
@@ -31,5 +28,6 @@ class UserStoryDetail {
         comment = json["comment"].stringValue
         totalPoints = json["total_points"].doubleValue
         statusExtraInfo = StatusExtraInfo(json: json["status_extra_info"])
+        assignedToExtraInfo = AssignedToExtraInfo(json: json["assigned_to_extra_info"])
     }
 }
